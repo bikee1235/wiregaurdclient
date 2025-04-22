fa5bc868ce34a74db5154b92b6b142a404ce946b
bikee1235 <bikee.bihari@sstsinc.com>
2025-04-22 18:04:00 +0530

    clean

--- src/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Routes module initialization.
-This module initializes the routes package for the Flask API.
-"""
-
-# This file can be empty or contain package-level imports and initialization
